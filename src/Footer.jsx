9f3e61f8c5f4f6e62135a196dfa2642e44097a74
Sofi Brugo <brugosofi@gmail.com>
2024-02-08 20:33:08 -0300

    git commit -m "creando componentes"

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.css";
+
+const footer = () => {
+  return (
+    <div className="footer">
+      <p>Created by: Sofia</p>
+      <a href="https://github.com/Sbrugo"></a>
+    </div>
+  );
+};
---
+++ src/Navbar.jsx
@@ -0,0 +1,25 @@
+import "./Navbar.css";
+
+const navbar = () => {
+  return (
+    <div className="navbar">
+      <a href="#about" className="nav-link">
+        About
+      </a>
+      <a href="home" className="nav-link">
+        Home
+      </a>
+      <button
+        onClick={() => (window.location.href = "/portfolio")}
+        classNam="btn btn-primary"
+      >
+        Portfolio
+      </button>
+      <a href="#contact" className="nav-link">
+        Contact
+      </a>
+    </div>
+  );
+};
+
+export default Navbar;
