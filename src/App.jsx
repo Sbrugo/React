d7412e53170c23cfbbb69f210d41679ed4501d97
Sofi Brugo <brugosofi@gmail.com>
2024-02-09 13:10:13 -0300

    "Agregando estilos"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Navbar from './Navbar'
+import "./App.css";
+import Navbar from "./Navbar.jsx";
+import Footer from ".Footer.jsx";
 
 function App() {
-  return 
-   <div>
-    <Navbar/>
-   </div>  
+  return;
+  <div>
+    <Navbar />
+    <Footer />
+  </div>;
 }
 
-export default App
+export default App;
